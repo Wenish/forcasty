aa8938aefaa2d04770b451030d7ca6cd2723953a
Wenish <Wenish@users.noreply.github.com>
2023-08-26 04:03:43 +0200

    Update PageNotFound.vue

--- forcasty-web/src/pages/PageNotFound.vue
+++ forcasty-web/src/pages/PageNotFound.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        Page Not Found
+    <div class="grid justify-center">
+        <h1 class="font-bold text-4xl">404</h1>
+        <p class="font-bold text-2xl">Page_Not_Found</p>
     </div>
 </template>
 <script setup lang="ts">
