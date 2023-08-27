3a816e75396c524ab221f159080c4e4e818ed7d0
Wenish <Wenish@users.noreply.github.com>
2023-08-27 22:44:23 +0200

    Update main.ts

--- forcasty-web/src/main.ts
+++ forcasty-web/src/main.ts
@@ -6,10 +6,18 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { firebaseConfig } from './firebaseConfig';
+import { getAuth } from 'firebase/auth';
 
-function bootstrap() {
+async function bootstrap() {
     const appFirebase = initializeApp(firebaseConfig);
     getAnalytics(appFirebase);
+    const auth = getAuth()
+    await new Promise((resolve) => {
+      const unsub = auth.onAuthStateChanged((user) => {
+        unsub()
+        resolve(user)
+      })
+    })
     const pinia = createPinia()
     const app = createApp(App)
     app.use(pinia)
