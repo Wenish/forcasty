fedb6b23befd2600be2799b73d20864fa8cc5a74
Wenish <Wenish@users.noreply.github.com>
2023-08-28 01:54:46 +0200

    Update ProjectForm.vue

--- forcasty-web/src/components/ProjectForm.vue
+++ forcasty-web/src/components/ProjectForm.vue
@@ -13,7 +13,7 @@
         </div>
         <div class="alert">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <span><b>Done</b> is the sum of all closed Storied at that moment of time. So this value should generally increase after each Sprint.</span>
+            <span><b>Done</b> is the sum of all closed Stories at that moment of time. So this value should generally increase after each Sprint.</span>
         </div>
         <div class="grid gap-4">
             <div v-for="(item, index) in timeline" :key="index">
