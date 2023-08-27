fc9032c6d213113114dbbd437591c4b0fddbc6df
Wenish <Wenish@users.noreply.github.com>
2023-08-27 03:03:54 +0200

    Update timeline.schema.ts

--- forcasty-api/src/database/schemas/timeline.schema.ts
+++ forcasty-api/src/database/schemas/timeline.schema.ts
@@ -1,10 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
 
 @Schema({
   _id: false,
 })
-export class Timeline extends Document {
+export class Timeline {
   @Prop()
   effort: number;
 
